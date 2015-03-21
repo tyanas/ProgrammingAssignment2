51aaedfa25241190064004f6df64390d5bb4cb85
tyanas <tyanas.go@gmail.com>
2015-03-22 00:34:40 +0300

    check changes in initial matrix; add some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there may be some
+# benefit to caching the inverse of a matrix rather than computing it
+# repeatedly. One can find here a pair of functions that cache the inverse
+# of a matrix.
 
-## Write a short comment describing this function
+
+# This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    # reset cache
     inv <- NULL
     set <- function(y) {
+        # set base matrix
         x <<- y
+        # clear cache
         inv <<- NULL
     }
     get <- function() x
+    # update cache
     setinverse <- function(inverse) inv <<- inverse
     getinverse <- function() inv
+
+    # "matrix" object interface
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# This function computes the inverse of the special "matrix" returned
+# by `makeCacheMatrix` above. If the inverse has already been calculated
+# (and the matrix has not changed), then `cacheSolve` will retrieve
+# the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
+# Assume `X` is a square invertible matrix
+
+cacheSolve <- function(x, currentX, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- x$getinverse()
-    if(!is.null(inv)) {
+    # first check if we have already cached object
+    # second (not evaluated when cache is empty) check are there any changes in
+    # in base matrix comared to current matrix
+    if(!is.null(inv) && all(x$get() == currentX)) {
         message("getting cached data")
         return(inv)
     }
-    data <- x$get()
     message("calculating data")
-
-    inv <- solve(data, ...)
+    x$set(currentX)
+    # caclucate inversed matrix here
+    inv <- solve(currentX, ...)
     x$setinverse(inv)
     inv
 }
 
 testCache <- function(N) {
-	x <- matrix(rnorm(N*N,mean=5,sd=1), N, N)
-    cm<-makeCacheMatrix(x)
-    cacheSolve(cm)
-    cacheSolve(cm)
+    # test how it goes
+    x <- matrix(rnorm(N*N, mean=5, sd=1), N, N)
+    # no calculations, just define an object
+    cm <- makeCacheMatrix(x)
+    # must be 'calculating data'
+    cacheSolve(cm, x)
+    # must be 'getting cached data'
+    cacheSolve(cm, x)
+    # change base matrix a bit
+    x[2,1] <- 10
+    # must be 'calculating data'
+    cacheSolve(cm, x)
+    # must be 'getting cached data'
+    cacheSolve(cm, x)
     'done'
 }
